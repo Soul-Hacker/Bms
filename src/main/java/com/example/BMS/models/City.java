b5f91b2fa09d6f728c3df6bb3fbac726e2f1eca5
Hemant Kumar Saw <67191552+Soul-Hacker@users.noreply.github.com>
2026-04-19 00:26:12 +0530

    Moveed to maven as facing some issues

--- src/main/java/com/example/BMS/models/City.java
+++ src/main/java/com/example/BMS/models/City.java
@@ -1,11 +1,12 @@
 package com.example.BMS.models;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
 @Entity
-public class City extends BaseModel {
+public class City extends BaseModel{
     private String name;
-
 }
